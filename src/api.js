15542dc74034932dc0b629111fb52a5c533b4df5
Thiago Silverio <127800106+thiago0-08@users.noreply.github.com>
2024-07-26 19:28:18 -0300

    Update api.js
    correção para do erro que não estava puxando a URL correta da api

--- src/api.js
+++ src/api.js
@@ -1,31 +1,27 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://localhost:7066', 
+  baseURL: 'https://localhost:7066',
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
-const port = process.env.PORT || 3301;
-
 export default {
   getProdutos() {
-    return apiClient.get('/produtos'); 
+    return apiClient.get('/produtos');
   },
 
   getProdutoById(id) {
-    return apiClient.get(`/produtos/${id}`); 
+    return apiClient.get(`/produtos/${id}`);
   },
 
   getCategorias() {
     return apiClient.get(`/categorias`);
   },
 
-  getProdutosByCategoria(idCategoria) { 
-    return apiClient.get(`/produtosPorCategoria/{IdCategoria}?id=${idCategoria}`); 
+  getProdutosByCategoria(idCategoria) {
+    
+    return apiClient.get(`/produtosPorCategoria/${idCategoria}`);
   }
-  
-
-
 };
